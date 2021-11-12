9bad65491b2560bae4a72900d09da6b8dad471ce
you2143 <your.00719@gmail.com>
2021-11-13 02:49:26 +0900

    Thymeleafを使えるように

--- server/src/main/kotlin/com/example/demo/HelloController.kt
+++ server/src/main/kotlin/com/example/demo/HelloController.kt
@@ -1,12 +1,14 @@
-package com.example.demo
+package com.example.demo.controller
  
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
- 
-@RestController
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.*
+
+@Controller
 class HelloController {
     @GetMapping("/")
-    fun hello(): String {
-        return "Hello World"
+    fun Hello(model: Model): String {
+        model.addAttribute("message", "Hello Test")
+        return "Hello"
     }
 }
